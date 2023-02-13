4ce1be098fa60363ab7601277a6a50131736f868
Yusup <hunzah16@gmail.com>
2023-02-13 11:42:02 +0300

    add modalWindow

--- js/script.js
+++ js/script.js
@@ -1,92 +1,142 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
+  
+	//TAB
+	const tabsParent = document.querySelector('.tabheader__items'),
+		tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent');
 
-  //TABS
-  const tabsParent = document.querySelector(".tabheader__items"),
-    tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent");
+	function hideTabContent () {
+		tabsContent.forEach((item) => {
+			item.classList.add ('hide');
+			item.classList.remove ('show', 'fade');
+		});
 
-  function hideTabContent () {
-    tabsContent.forEach((item) => {
-      item.classList.add ('hide');
-      item.classList.remove ('show', 'fade');
-    });
+		tabs.forEach((item) => {
+			item.classList.remove('tabheader__item_active');
+		});
+	}
 
-    tabs.forEach((item) => {
-      item.classList.remove("tabheader__item_active");
-    });
-  }
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add ('show', 'fade');
+		tabsContent[i].classList.remove ('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
 
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add ('show', 'fade');
-    tabsContent[i].classList.remove ('hide');
-    tabs[i].classList.add("tabheader__item_active");
-  }
+	hideTabContent();
+	showTabContent();
 
-  hideTabContent();
-  showTabContent();
+	tabsParent.addEventListener ('click', (event) =>{
+		const target = event.target;
+		tabs.forEach((item, i) =>{
+			if (target == item) {
+				hideTabContent();
+				showTabContent(i);
+			}
+		});
+	});
 
-tabsParent.addEventListener ('click', (event) =>{
-    const target = event.target;
-    tabs.forEach((item, i) =>{
-        if (target == item) {
-            hideTabContent();
-            showTabContent(i);  
-        }
-    })
-})
+	// TIMER
+	const deadline = '2023-02-19';
+
+	function getTimeRemaining(endtime) {
+		let days, hours, minutes,seconds;
+		const t = Date.parse(endtime) - Date.parse(new Date());
+		if(t<=0) {
+			days = 0;
+			hours = 0;
+			minutes = 0;
+			seconds = 0;
+		}
+		else {
+			days = Math.floor( (t/(1000*60*60*24)) );
+			seconds = Math.floor( (t/1000) % 60 );
+			minutes = Math.floor( (t/1000/60) % 60 );
+			hours = Math.floor( (t/(1000*60*60) % 24) );
+		}
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+	function getZero(num){ 
+		if (num >= 0 && num < 10) {
+			return '0' + num;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('.timer', deadline);
 
 
-//TIMER
-const deadline = '2023-02-19';
+	// MODALWINDOW
+	const btns = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector ('.modal'),
+		modalWindowClose = document.querySelector ('.modal__close');
 
-function getTimeRemaining(endtime) {
-  const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor( (t/(1000*60*60*24)) ),
-      seconds = Math.floor( (t/1000) % 60 ),
-      minutes = Math.floor( (t/1000/60) % 60 ),
-      hours = Math.floor( (t/(1000*60*60) % 24) );
+	function openModal (){
+		modal.classList.add ('show');
+		modal.classList.remove ('hide');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+	btns.forEach(btn => {
+		btn.addEventListener('click', openModal);
+		
+	});
 
-      return {
-          'total': t,
-          'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
-      };
-  }
-  function getZero(num){
-      if (num >= 0 && num < 10) { 
-          return '0' + num;
-      } else {
-          return num;
-      }
-  }
+	function closeModal(){
+		modal.classList.remove ('show');
+		modal.classList.add ('hide');
+		document.body.style.overflow = '';
+	}
 
-  function setClock(selector, endtime) {
+	modalWindowClose.addEventListener ('click', closeModal);
 
-      const timer = document.querySelector(selector),
-          days = timer.querySelector("#days"),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timeInterval = setInterval(updateClock, 1000);
+	modal.addEventListener('click', closeModal(), (e) => {
+		if (e.target === modal)
+			closeModal();
+	});
+	document.addEventListener ('keydown',(e)=>{
+		if (e.code=== 'Escape'&& modal.classList.contains('show')){
+			closeModal();
+		}
+	});
+	const modalTimerId = setTimeout(openModal, 3000);
 
-      updateClock();
-
-      function updateClock() {
-          const t = getTimeRemaining(endtime);
-
-          days.innerHTML = getZero(t.days);
-          hours.innerHTML = getZero(t.hours);
-          minutes.innerHTML = getZero(t.minutes);
-          seconds.innerHTML = getZero(t.seconds);
-
-          if (t.total <= 0) {
-              clearInterval(timeInterval);
-          }
-      }
-  }
-
-  setClock('.timer', deadline);
-
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+	window.addEventListener('scroll', showModalByScroll);
 });

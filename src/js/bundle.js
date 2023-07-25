532117aa2ba7ce92419d6f44a2754796b9a2a756
Yusup <hunzah9@mail.ru>
2023-07-25 21:17:37 +0300

    commit

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -746,7 +746,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_0__["default"])('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     (0,_modules_modal__WEBPACK_IMPORTED_MODULE_1__["default"])('[data-modal]', '.modal', modalTimerId);
-    (0,_modules_timer__WEBPACK_IMPORTED_MODULE_2__["default"])('.timer', '2022-06-11');
+    (0,_modules_timer__WEBPACK_IMPORTED_MODULE_2__["default"])('.timer', new Date());
     (0,_modules_cards__WEBPACK_IMPORTED_MODULE_3__["default"])();
     (0,_modules_calc__WEBPACK_IMPORTED_MODULE_4__["default"])();
     (0,_modules_forms__WEBPACK_IMPORTED_MODULE_5__["default"])('form', modalTimerId);
@@ -761,6 +761,7 @@ window.addEventListener('DOMContentLoaded', function () {
         field: '.offer__slider-inner'
     });
 });
+
 })();
 
 /******/ })()
